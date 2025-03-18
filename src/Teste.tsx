fdd294ee8248f0f76be97d8eb7b46a4018aff19c
lucas barros <lucasgleyfson59@gmail.com>
2025-03-18 12:33:02 -0300

    Configurando o ambiente de desenvolvimento

---
+++ src/Teste.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  color: red;
+  cursor: pointer;
+  backgr
+`
+
+function Teste() {
+  return <Botao>lucas</Botao>
+}
+
+export default Teste
