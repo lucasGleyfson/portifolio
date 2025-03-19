e7588598c747aeadeae30692ca3eb1b335aa78e8
lucas barros <lucasgleyfson59@gmail.com>
2025-03-18 21:20:02 -0300

    Estilizando a SideBar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/lucasGleyfson.png" />
-    <Titulo fontSize={20}>Lucas Barros</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas Barros</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Lucas Barros
+      </Paragrafo>
+      <Descricao tipo="secundario" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
